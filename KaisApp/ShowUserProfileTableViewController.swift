14d203b80fe2662b21070fedb11404589c3013cc
Elena Caballero <elenacaballero@Elenas-MacBook-Pro.local>
2017-12-07 17:26:44 -0600

    Adding unfollow functionality

--- KaisApp/ShowUserProfileTableViewController.swift
+++ KaisApp/ShowUserProfileTableViewController.swift
@@ -40,7 +40,14 @@ class ShowUserProfileTableViewController: UITableViewController {
         self.hideKeyboardWhenTappedAround()
         
         if showUserProfileTableView != nil {
-            showUserProfileTableView.backgroundView = activityIndicatorView
+            showUserProfileTableView.addSubview(activityIndicatorView)
+            activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+            activityIndicatorView.hidesWhenStopped = true
+            activityIndicatorView.color = UIColor.black
+            let horizontalConstraint = NSLayoutConstraint(item: activityIndicatorView, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+            showUserProfileTableView.addConstraint(horizontalConstraint)
+            let verticalConstraint = NSLayoutConstraint(item: activityIndicatorView, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)
+            showUserProfileTableView.addConstraint(verticalConstraint)
         
             showUserProfileTableView.register(UINib.init(nibName: "MainDetailHeader", bundle: Bundle.main), forHeaderFooterViewReuseIdentifier: "MainDetailHeaderID")
         }
@@ -52,14 +59,6 @@ class ShowUserProfileTableViewController: UITableViewController {
         
         navigationController?.navigationBar.barTintColor = UIColor(rgb: 0x2390D4)
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
-        
-        let barButton = UIButton(type: .custom)
-        barButton.setTitle("Seguir", for: .normal)
-        barButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-        barButton.addTarget(self, action: #selector(followUser), for: .touchUpInside)
-        let item = UIBarButtonItem(customView: barButton)
-        
-        self.navigationItem.setRightBarButton(item, animated: true)
         
         if snapshots.isEmpty {
             activityIndicatorView.startAnimating()
@@ -74,11 +73,31 @@ class ShowUserProfileTableViewController: UITableViewController {
                     
                     self.loadImageContentForCells()
                     self.loadUserContent()
+                    self.checkIfFollowing()
                 }
             }
         }
         
+    }
+    
+    func checkIfFollowing() {
+        ref = Database.database().reference(fromURL: "https://kaisapp-dev.firebaseio.com")
         
+        let userAuthUID = (Auth.auth().currentUser?.uid)!
+        let userShownID = uid
+        
+        ref.child("follows/\(userAuthUID)/following").queryOrderedByKey().observe(.value, with: { [weak self] (snapshot) in
+            if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshots {
+                    if snap.key.elementsEqual(userShownID) {
+                        self?.changeBarButton(when: false)
+                    }else {
+                        self?.changeBarButton(when: true)
+                    }
+                }
+            }
+            
+        })
     }
     
     @objc func followUser(){
@@ -92,77 +111,145 @@ class ShowUserProfileTableViewController: UITableViewController {
         var userShownUName:String = String()
         var followers:Int = Int()
         
-        ref.child("users").child(userAuthUID).observeSingleEvent(of: .value, with: { [weak self] (snapshot) in
-            
-            //getting auth info
-            if let value = snapshot.value as? Dictionary<String, AnyObject> {
-                if value["uname"] as? String != nil {
-                    userAuthUName = (value["uname"] as? String)!
-                }else {
-                    userAuthUName = (Auth.auth().currentUser?.email)!
-                }
-                if value["following"] as? Int != nil {
-                    following = (value["following"] as? Int)! + 1
-                }else {
-                    following = 1
-                }
-            }
-            
-            //getting shown info
-            if let value = self?.users.value as? Dictionary<String, AnyObject> {
-                if value["uname"] as? String != nil {
-                    userShownUName = (value["uname"] as? String)!
-                }else {
-                    userShownUName = (Auth.auth().currentUser?.email)!
-                }
-                if value["followers"] as? Int != nil {
-                    followers = (value["followers"] as? Int)! + 1
-                }else {
-                    followers = 1
-                }
-            }
-            
-            //setting following of auth user
-            self?.ref.child("users/\(userAuthUID)/following").setValue(following)
-            
-            let userShown = ["\(userShownID)": userShownUName]
-            
-            //setting following of userauth
-            self?.ref.child("follows").queryOrderedByKey().observe(.value, with: { [weak self] (snapshot) in
-                if let snap = snapshot.value as? Dictionary<String, AnyObject> {
-                    if snap.keys.contains(userAuthUID) {
-                        self?.ref.child("follows/\(userAuthUID)/followers").queryOrderedByKey().observe(.value, with: { [weak self] (snapshot) in
-                            if let snap = snapshot.value as? Dictionary<String, AnyObject> {
-                                if snap.keys.contains(userShownID) {
-                                    self?.ref.child("follows/\(userAuthUID)/following").updateChildValues(userShown)
-                                }else {
-                                    self?.ref.child("follows/\(userAuthUID)/following").updateChildValues(userShown)
-                                }
-                            }
-                        })
-                    }else {
-                       self?.ref.child("follows").child(userAuthUID).child("following").setValue(userShown)
-                    }
-                }
-            })
-            
-            //setting followers user shown
-            self?.ref.child("users/\(userShownID)/followers").setValue(followers)
-            
-            let userAuth = ["\(userAuthUID)": userAuthUName]
-            
-            //setting followers usershown
-            self?.ref.child("follows/\(userShownID)/followers").queryOrderedByKey().observe(.value, with: { [weak self] (snapshot) in
-                if let snap = snapshot.value as? Dictionary<String, AnyObject> {
-                    if snap.keys.contains(userAuthUID) {
-                        self?.ref.child("follows/\(userShownID)/followers").updateChildValues(userAuth)
-                    }else {
-                        self?.ref.child("follows/\(userShownID)/followers").updateChildValues(userAuth)
-                    }
-                }
-            })
+        if navigationItem.rightBarButtonItem?.title?.caseInsensitiveCompare("Seguir") == ComparisonResult.orderedSame  {
+            ref.child("users").child(userAuthUID).observeSingleEvent(of: .value, with: { [weak self] (snapshot) in
                 
-        })
+                //getting auth info
+                if let value = snapshot.value as? Dictionary<String, AnyObject> {
+                    if value["uname"] as? String != nil {
+                        userAuthUName = (value["uname"] as? String)!
+                    }else {
+                        userAuthUName = (Auth.auth().currentUser?.email)!
+                    }
+                    if value["following"] as? Int != nil {
+                        following = (value["following"] as? Int)! + 1
+                    }else {
+                        following = 1
+                    }
+                }
+                
+                //getting shown info
+                if let value = self?.users.value as? Dictionary<String, AnyObject> {
+                    if value["uname"] as? String != nil {
+                        userShownUName = (value["uname"] as? String)!
+                    }else {
+                        userShownUName = (Auth.auth().currentUser?.email)!
+                    }
+                    if value["followers"] as? Int != nil {
+                        followers = (value["followers"] as? Int)! + 1
+                    }else {
+                        followers = 1
+                    }
+                }
+                
+                //setting following of auth user
+                self?.ref.child("users/\(userAuthUID)/following").setValue(following)
+                
+                let userShown = ["\(userShownID)": userShownUName]
+                
+                //setting following of userauth
+                self?.ref.child("follows").queryOrderedByKey().observe(.value, with: { [weak self] (snapshot) in
+                    if let snap = snapshot.value as? Dictionary<String, AnyObject> {
+                        if snap.keys.contains(userAuthUID) {
+                            self?.ref.child("follows/\(userAuthUID)/followers").queryOrderedByKey().observe(.value, with: { [weak self] (snapshot) in
+                                if let snap = snapshot.value as? Dictionary<String, AnyObject> {
+                                    if snap.keys.contains(userShownID) {
+                                        self?.ref.child("follows/\(userAuthUID)/following").updateChildValues(userShown)
+                                    }else {
+                                        self?.ref.child("follows/\(userAuthUID)/following").updateChildValues(userShown)
+                                    }
+                                }
+                            })
+                        }else {
+                            self?.ref.child("follows").child(userAuthUID).child("following").setValue(userShown)
+                        }
+                    }
+                })
+                
+                //setting followers user shown
+                self?.ref.child("users/\(userShownID)/followers").setValue(followers)
+                
+                let userAuth = ["\(userAuthUID)": userAuthUName]
+                
+                //setting followers usershown
+                self?.ref.child("follows/\(userShownID)/followers").queryOrderedByKey().observe(.value, with: { [weak self] (snapshot) in
+                    if let snap = snapshot.value as? Dictionary<String, AnyObject> {
+                        if snap.keys.contains(userAuthUID) {
+                            self?.ref.child("follows/\(userShownID)/followers").updateChildValues(userAuth)
+                        }else {
+                            self?.ref.child("follows/\(userShownID)/followers").updateChildValues(userAuth)
+                        }
+                    }
+                })
+                
+            })
+            
+            changeBarButton(when: true)
+            
+        }else {
+            ref.child("users").child(userAuthUID).observeSingleEvent(of: .value, with: { [weak self] (snapshot) in
+                
+                //getting auth info
+                if let value = snapshot.value as? Dictionary<String, AnyObject> {
+                    if value["uname"] as? String != nil {
+                        userAuthUName = (value["uname"] as? String)!
+                    }else {
+                        userAuthUName = (Auth.auth().currentUser?.email)!
+                    }
+                    if value["following"] as? Int != nil {
+                        following = (value["following"] as? Int)! - 1
+                    }else {
+                        following = 0
+                    }
+                }
+                
+                //getting shown info
+                if let value = self?.users.value as? Dictionary<String, AnyObject> {
+                    if value["uname"] as? String != nil {
+                        userShownUName = (value["uname"] as? String)!
+                    }else {
+                        userShownUName = (Auth.auth().currentUser?.email)!
+                    }
+                    if value["followers"] as? Int != nil {
+                        followers = (value["followers"] as? Int)! - 1
+                    }else {
+                        followers = 0
+                    }
+                }
+                
+                //setting following of auth user
+                self?.ref.child("users/\(userAuthUID)/following").setValue(following)
+                
+                //setting followers user shown
+                self?.ref.child("users/\(userShownID)/followers").setValue(followers)
+            })
+            
+            changeBarButton(when: false)
+        }
+    }
+    
+    func changeBarButton(when following: Bool) {
+        if following {
+            navigationItem.rightBarButtonItem = nil
+            
+            let barButton = UIButton(type: .custom)
+            barButton.setTitle("No Seguir", for: .normal)
+            barButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            barButton.addTarget(self, action: #selector(followUser), for: .touchUpInside)
+            let item = UIBarButtonItem(customView: barButton)
+            
+            navigationItem.setRightBarButton(item, animated: true)
+        }else {
+            navigationItem.rightBarButtonItem = nil
+            
+            let barButton = UIButton(type: .custom)
+            barButton.setTitle("Seguir", for: .normal)
+            barButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            barButton.addTarget(self, action: #selector(followUser), for: .touchUpInside)
+            let item = UIBarButtonItem(customView: barButton)
+            
+            navigationItem.setRightBarButton(item, animated: true)
+        }
     }
 
     // MARK: - Table view data source
@@ -177,7 +264,7 @@ class ShowUserProfileTableViewController: UITableViewController {
             return 1
         case 1:
             var countSnapshots = 0
-            if snapshots.count > 0 {
+            if snapshots.count != 0 {
                 TableViewHelper.EmptyMessage(message: "", viewController: self)
                 for snap in snapshots{
                     let thing = snap.value as? Dictionary<String, AnyObject>
@@ -189,8 +276,7 @@ class ShowUserProfileTableViewController: UITableViewController {
                 }
                 return countSnapshots
             }else {
-                TableViewHelper.EmptyMessage(message: "Aún no hay fotografías,\n agrega una y muestra la magia de tus viajes.", viewController: self)
-                return 0
+                return 1
             }
         default:
             assert(false, "section \(section)")
@@ -246,7 +332,7 @@ class ShowUserProfileTableViewController: UITableViewController {
         
         var cell:  UserProfileTableViewCell
         
-        if imagesSnapshots.count > 0 {
+        if imagesSnapshots.count != 0 {
             if indexPath.row >= imagesSnapshots.count {
                 cell = tableView.dequeueReusableCell(withIdentifier: "showEmptyImagesArea", for: indexPath) as! UserProfileTableViewCell
             }else {
